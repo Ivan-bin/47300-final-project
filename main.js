0431b1bf4bf5e7aa76d5755cbc711ed73d10aa20
JiandeLi1 <47312442+JiandeLi1@users.noreply.github.com>
2020-09-30 15:59:49 -0400

    The Home page and Login page demo

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// $('.slider-one').slick();
+// .not(".slick-intialized")
+// .slick({
+// autoplay:true,
+// autoplaySpeed:3000,
+// dots:true
+// });
+
+
+      // window.onload = function(){
+      // var imgList = ducument.getElementById("img-list");
+      // var imgArr = document.getElementsByTagName("img");
+      // imgList.style.width = 1173 * imgArr.length + "px";
+      // }
+   
+      // var x = document.getElementById("login");
+      // var y = document.getElementById("register");
+      // var z = document.getElementById("L-R-btn");
+
+      // function register(){
+      //       x.style.left="-400px";
+      //       y.style.left="50px";
+      //       z.style.left="110px";
+      // }
